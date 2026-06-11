01490f53a2e62a0b8c907f05ff9311f9bd063a1f
TalkRally <106599866+TalkRally@users.noreply.github.com>
2026-06-11 06:34:16 -0500

    Phase 2: Add patterns/header-page.php with header block pattern

---
+++ patterns/header-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: Page Header
+ * Slug: talkrally/header-page
+ * Categories: talkrally-sections, talkrally-headers
+ * Description: Reusable TalkRally header section with logo, site title, tagline, and navigation.
+ * Keywords: header, site logo, site title, navigation, page header, TalkRally
+ * Viewport Width: 1200
+ *
+ * @package TalkRally
+ */
+?>
+
+<!-- wp:group {"tagName":"header","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"support","layout":{"type":"default"}} -->
+<header class="wp-block-group has-support-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|20","left":"var:preset|spacing|20","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","backgroundColor":"secondary","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide has-secondary-background-color has-background"><!-- wp:site-logo {"width":463,"align":"center","className":"is-style-default","style":{"layout":{"selfStretch":"fill","flexSize":null}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"}},"backgroundColor":"deep-blue","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide has-deep-blue-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:navigation {"ref":4,"textColor":"background","align":"full","style":{"spacing":{"blockGap":"3.5rem"}},"fontSize":"body","layout":{"type":"flex","justifyContent":"center"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"layout":{"type":"constrained","justifyContent":"right"}} -->
+<div class="wp-block-group alignwide" style="padding-top:0;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":80,"textColor":"border-subtle","backgroundColor":"support","style":{"spacing":{"blockGap":"0rem"},"typography":{"fontSize":"0.95rem"}},"layout":{"type":"flex","justifyContent":"center"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></header>
+<!-- /wp:group -->
